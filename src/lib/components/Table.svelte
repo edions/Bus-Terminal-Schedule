eeab67fa81b305a9d20bb96a830edcc9d4271910
EdisonP <133362311+edions@users.noreply.github.com>
2023-11-15 02:30:59 +0800

    Adjustment

---
+++ src/lib/components/Table.svelte
@@ -0,0 +1,53 @@
+<script>
+    export let columns = []; // Array of column headers
+    export let data = []; // Array of data rows
+  
+    // Function to handle sorting
+    let sortByColumn = (column) => {
+      // Implement your sorting logic here
+      console.log(`Sorting by ${column}`);
+    };
+  </script>
+  
+  <style>
+    table {
+      width: 100%;
+      border-collapse: collapse;
+      margin-top: 20px;
+    }
+  
+    th, td {
+      border: 1px solid #ddd;
+      padding: 8px;
+      text-align: left;
+    }
+  
+    th {
+      background-color: #f2f2f2;
+      cursor: pointer;
+    }
+  
+    tr:hover {
+      background-color: #f5f5f5;
+    }
+  </style>
+  
+  <table>
+    <thead>
+      <tr>
+        {#each columns as column (column)}
+          <th on:click={() => sortByColumn(column)}>{column}</th>
+        {/each}
+      </tr>
+    </thead>
+    <tbody>
+      {#each data as row (row.id)}
+        <tr>
+          {#each columns as column (column)}
+            <td>{row[column]}</td>
+          {/each}
+        </tr>
+      {/each}
+    </tbody>
+  </table>
+  
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,13 @@
 <script>
     import hero_image from '$lib/images/hero-image.jpg';
+    import Table from '$lib/components/Table.svelte'
+
+    let columns = ['Name', 'Age', 'Country'];
+  let data = [
+    { id: 1, Name: 'John Doe', Age: 30, Country: 'USA' },
+    { id: 2, Name: 'Jane Doe', Age: 25, Country: 'Canada' },
+    { id: 3, Name: 'Bob Smith', Age: 35, Country: 'UK' },
+  ];
 </script>
 
 <svelte:head>
@@ -9,12 +17,13 @@
 
 <img class="hero-img" src={hero_image} alt="Welcome" />
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>SCHEDULE <br>Live bus travel information</h1>
+
+<Table {columns} {data} />
 
 <style>
     .hero-img {
         width: 100%;
-        height: 500px;
+        height: 640px;
     }
 </style>
