7cfff0f2d23b6ae459c7173159cdb9c7d3add13a
EdisonP <133362311+edions@users.noreply.github.com>
2023-11-14 12:31:04 +0800

    Minor Fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,20 @@
+<script>
+    import hero_image from '$lib/images/hero-image.jpg';
+</script>
+
+<svelte:head>
+    <title>Home</title>
+    <meta name="description" content="Bus" />
+</svelte:head>
+
+<img class="hero-img" src={hero_image} alt="Welcome" />
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style>
+    .hero-img {
+        width: 100%;
+        height: 500px;
+    }
+</style>
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,7 +1,7 @@
 <header>
     <div class="comp">
         <a href="/" class="comp-name">
-            <h2 style="margin-left: -5.8rem;"><span>BUSSIN</span></h2>
+            <h2><span>BUSSIN</span></h2>
         </a>
     </div>
 
@@ -10,59 +10,81 @@
         <li><a href="/">TERMINAL FACILITIES</a></li>
         <li><a href="/">LOST AND FOUND</a></li>
     </ul>
+
+    <div class="social-icons">
+        <p>f</p>
+        <a href="https://twitter.com" class="social-icon"><img src="/path/to/twitter-icon.png" alt="Twitter">f</a>
+    </div>
 </header>
 
 <style>
-    header{
+    header {
         position: fixed;
         width: 100%;
         top: 0;
-        right: 0;
+        left: 0;
         z-index: 1000;
         display: flex;
         align-items: center;
-        justify-content: space-between;
         background: transparent;
-        padding: 20px 15%;
+        padding: 20px 5%;
         border-bottom: 1px solid transparent;
         transition: all 0.45s ease;
-        padding-left: 8.5%;
-        padding-right: 8.5%;
-        font-weight: 600;
     }
 
-    .comp-name{
+    .comp-name {
         font-weight: 700;
         top: -30px;
         padding-left: 9px;
+        margin-right: 25px;
+        margin-left: -50px;
+        text-decoration: none; 
     }
 
-    .comp{
+    .comp {
         display: flex;
-        justify-items: center;
+        align-items: center;
     }
 
-    .navlist{
+    .navlist {
         display: flex;
-        margin-left: -15rem;
+        list-style: none;
+        padding: 0;
     }
 
-    .navlist a{
-        color: var(--v-dark-purple);
-        font-size: var(--small-font);
-        font-weight:  600;
-        margin: 0 25px;
+    .navlist li {
+        margin-right: 25px;
+    }
+
+    .navlist a {
+        font-weight: 600;
         transition: all 0.45s ease;
-
         color: #0D0111;
         font-family: Poppins;
         font-size: 15.5px;
         font-style: normal;
-        font-weight: 600;
         line-height: normal;
+        text-decoration: none; 
     }
 
-    .navlist a:hover{
+    .navlist a:hover {
         color: rgb(43, 198, 107);
+    }
+
+    .social-icons {
+        display: flex;
+        margin-left: auto; /* Move social icons to the right */
+    }
+
+    .social-icon {
+        margin-left: 15px;
+        display: flex;
+        align-items: center;
+        text-decoration: none;
+    }
+
+    .social-icon img {
+        width: 20px; /* Adjust the width of the social icons */
+        height: 20px; /* Adjust the height of the social icons */
     }
 </style>
